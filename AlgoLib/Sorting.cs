d1273303fb51b1448688251e79f4f0df6c4dbcd3
Denis Lebedev <dv.lebedev777@gmail.com>
2023-10-24 17:27:17 +0400

    add code

--- AlgoLib/Sorting.cs
+++ AlgoLib/Sorting.cs
@@ -8,9 +8,37 @@ namespace AlgoLib
 {
     public static class Sorting
     {
-        public static void Quik(int[] values)
+        public static int[] Quik(int[] array, int leftIndex, int rightIndex)
         {
-            throw new NotImplementedException();
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                SortArray(array, leftIndex, j);
+            if (i < rightIndex)
+                SortArray(array, i, rightIndex);
+            return array;
         }
     }
 }
