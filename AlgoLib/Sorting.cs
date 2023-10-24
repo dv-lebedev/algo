b7e9cd6d81c6b895de6f8eef3b7c9e2496b81e4d
Denis Lebedev <dv.lebedev777@gmail.com>
2023-10-24 17:25:44 +0400

    add quik

---
+++ AlgoLib/Sorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoLib
+{
+    public static class Sorting
+    {
+        public static void Quik(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
